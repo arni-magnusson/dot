35ab04f3d6125978a62b50af4f55778064f0908b
Arni Magnusson <arnima@hafro.is>
2017-11-23 11:55:09 +0100

    Comment out frame-maximize-x

--- .emacs-linux.el
+++ .emacs-linux.el
@@ -51,9 +51,9 @@
 ;;   (let ((maxcols (+ (truncate (/ (display-pixel-width)(frame-char-width))) 0))
 ;;         (maxrows (+ (truncate (/ (display-pixel-height)(frame-char-height))) 0)))
 ;;     (set-frame-size (selected-frame) maxcols maxrows)(set-frame-position (selected-frame) 0 0)))
-(defun frame-maximize-x () "Maximize frame." (interactive) ; alternative approach, smaller frame and inconsistent
-       (x-send-client-message nil 0 nil "_NET_WM_STATE" 32 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
-       (x-send-client-message nil 0 nil "_NET_WM_STATE" 32 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0)))
+;; (defun frame-maximize-x () "Maximize frame." (interactive) alternative approach, smaller frame and inconsistent
+;;        (x-send-client-message nil 0 nil "_NET_WM_STATE" 32 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
+;;        (x-send-client-message nil 0 nil "_NET_WM_STATE" 32 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0)))
 ;; (defun frame-restore () "Restore window." (interactive)(ignore))
 (defun frame-maximize () "Maximize window." (interactive)(modify-frame-parameters nil '((fullscreen . maximized))))
 (defun frame-restore () "Restore window." (interactive)(modify-frame-parameters nil '((fullscreen . nil))))
